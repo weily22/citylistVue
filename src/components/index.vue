43abeead3aecce03c670ff3288fee910b9dba650
wliyan <wly85961@gmail.com>
2017-07-09 13:56:22 +0800

    'init'

---
+++ src/components/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <group>
+      <cell title="上海 GPS"></cell>
+    </group>
+  </div>
+</template>
+<script>
+import {Group, Cell} from 'vux'
+export default {
+  components: {
+    Group,
+    Cell
+  }
+}
+</script>
+<style>
+
+</style>
